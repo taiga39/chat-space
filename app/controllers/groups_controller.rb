9a4ee7ef1ea2552a1714abad7b73c79318b2d581
taiga39 <Taiga@taiga.local>
2018-11-08 15:23:38 +0900

    fix indent

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,7 +8,6 @@ before_action :set_group, only: [:edit, :update]
     @group = Group.new
     @group.users << current_user
   end
-
   def create
     @group = Group.new(group_params)
     @group.users << current_user
@@ -18,7 +17,6 @@ before_action :set_group, only: [:edit, :update]
       render :new
     end
   end
-
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
@@ -26,12 +24,10 @@ before_action :set_group, only: [:edit, :update]
       render :edit
     end
   end
-
   private
   def group_params
     params.require(:group).permit(:name, { user_ids: [] })
   end
-
   def set_group
     @group = Group.find(params[:id])
   end
