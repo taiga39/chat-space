6a2385ed9f9bbc2892208a566ad3adb79e084d4c
taiga39 <Taiga@taiga.local>
2018-11-19 23:50:45 +0900

    edit group_member view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,18 +11,15 @@ class GroupsController < ApplicationController
   end
   def create
     @group = Group.new(group_params)
-    @group.users << current_user
     if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
+      redirect_to group_messages_path(@group), notice: 'グループを作成しました'
     else
       render :new
     end
   end
   def edit
-    @group = Group.find(params[:id])
   end
   def update
-
     if @group.update(group_params)
        redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
@@ -31,7 +28,7 @@ class GroupsController < ApplicationController
   end
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [])
+    params.require(:group).permit(:name,{:user_ids => [] })
   end
   def set_group
     @group = Group.find(params[:id])
